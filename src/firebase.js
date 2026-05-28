45b34c8d037598d3f6e5a08446908c56ac6704eb
ismaelbcs <ismaelbcs@gmail.com>
2026-05-27 21:30:40 -0700

    puerba 9 correos

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,21 @@
+// 1. Las importaciones siempre van HASTA ARRIBA del archivo
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// 2. Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-proyecto.firebaseapp.com",
-  projectId: "tu-proyecto",
-  storageBucket: "tu-proyecto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyAx6qoFqOmunIEgn7YomDdy80Pnquvh6AY",
+  authDomain: "ballardtours.firebaseapp.com",
+  databaseURL: "https://ballardtours-default-rtdb.firebaseio.com",
+  projectId: "ballardtours",
+  storageBucket: "ballardtours.firebasestorage.app",
+  messagingSenderId: "895636049481",
+  appId: "1:895636049481:web:ede542e568698d398af998",
 };
+
+// 3. Inicializamos Firebase (Esto te faltaba)
+const app = initializeApp(firebaseConfig);
+
+// 4. 👇 ESTA ES LA SOLUCIÓN DEL PASO 2 👇
+// Inicializamos la base de datos y la exportamos como 'db'
+export const db = getFirestore(app);
