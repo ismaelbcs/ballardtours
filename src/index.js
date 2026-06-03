7597ff16784b3e52ec3b67168eed748142b5f80b
ismaelbcs <ismaelbcs@gmail.com>
2026-06-03 15:21:23 -0700

    Rollback de react-snap y fix de index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
-import './index.css'; // Asegúrate de que el nombre de tu archivo CSS coincida
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-
-// Verificamos si react-snap ya generó el HTML para Google
-if (container.hasChildNodes()) {
-  // Si hay HTML previo (Prerender), solo lo "hidratamos"
-  hydrateRoot(
-    container,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  // Si es un navegador normal y no hay HTML, lo renderizamos desde cero
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
