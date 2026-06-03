e5905f43579a872891c0d83baaad1c051acd6fc8
ismaelbcs <ismaelbcs@gmail.com>
2026-06-03 16:12:49 -0700

    arreglango co

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 👈 1. Importamos el Enrutador
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 👈 2. Abrazamos tu App con el Enrutador */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
