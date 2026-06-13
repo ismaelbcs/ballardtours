e545e1de6eba56ce8b405cbbbc82aeadc76851ab
ismaelbcs <ismaelbcs@gmail.com>
2026-06-13 00:15:42 -0700

    arreglango co

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot, createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // 👈 1. Importamos el Enrutador
 import './index.css';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
 
-root.render(
+// Empaquetamos tu app con el enrutador para poder usarla en ambos casos
+const appContent = (
   <React.StrictMode>
     <BrowserRouter> {/* 👈 2. Abrazamos tu App con el Enrutador */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// Lógica de React 18 para detectar HTML estático de react-snap
+if (container.hasChildNodes()) {
+  // Si el HTML ya fue generado (Ideal para que Google lo lea al instante)
+  hydrateRoot(container, appContent);
+} else {
+  // Renderizado normal si no hay HTML previo
+  const root = createRoot(container);
+  root.render(appContent);
+}
